ccae88cb4540f20219d9478bb156ca194938fd17
PashaTravkin <refueller2022@bk.ru>
2022-05-03 00:00:17 +0300

    vdsffs

---
+++ index.js
@@ -0,0 +1,24 @@
+function countPositivesSumNegatives(input) {
+
+    let number = 0;
+    let y;
+    let sum = 0;
+    let i;
+    if (input != null) {
+        for (y = 0; y < input.length; y++) {
+            if (input[y] > 0) {
+                number++;
+            }
+        }
+        for (i = 0; i < input.length; i++) {
+            if (input[i] < 0) {
+                sum += input[i];
+            }
+        }
+        if (number == 0 && sum == 0) {
+            return [];
+        } else return [number, sum];
+    } else {
+        return [];
+    }
+}
